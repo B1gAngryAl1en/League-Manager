ca17456d101808706e6f93e944bf6ae724637df4
Mark <111181831+B1gAngryAl1en@users.noreply.github.com>
2023-01-14 11:38:25 +0000

    set up output data folder

--- process_results.py
+++ process_results.py
@@ -2,53 +2,56 @@ import pandas as pd
 
 from League import League
 
-league_name = "test_league"
+# set the league name
+league_name = "test league"
 
-# set up league, pass win_pts, draw_pts  and loss_pts args to override defaults (3,1,0)
-league = League(name=league_name)
+# set up league instance
+league = League(name=league_name, win_pts=3, draw_pts=1, loss_pts=0)
 
-# set up players, list of lists [name, team_name, note]
-players = [['player_a_name', 'player_a_team_name', 'player_a_note'],
-           ['player_b_name', 'player_b_team_name', 'player_b_note'],
-           ['player_c_name', 'player_c_team_name', 'player_c_note'],
-           ['player_d_name', 'player_d_team_name', 'player_d_note']]
+# get player list and add players
+players_df = pd.read_csv('input_data/player_list.csv')
 
-for player in players:
-    if not league.add_player(name=player[0], faction=player[1], note=player[2]):
-        print(f"error: player {player} not added")
+for (idx, player) in players_df.iterrows():
+    if player.loc['player name'] != "":
+        name = player.loc['player name']
+        team = player.loc['player team name']
+        note = player.loc['player note']
+        if not league.add_player(name=name, faction=team, note=note):
+            print(f"error: player {player} not added")
 
-# get game results
+# get game results and add them
 results_data_df = pd.read_csv('input_data/fixtures.csv')
 
-for (idx, row) in results_data_df.iterrows():
-    if row.loc['played'] == 'y':
-        rnd = row.loc['round']
-        game = row.loc['game']
-        player_a = row.loc['player_a']
-        player_b = row.loc['player_b']
-        player_a_score = row.loc['player_a_sc']
-        player_b_score = row.loc['player_b_sc']
-        result = league.add_result(rnd=rnd, game=game,
-                                   pl_a=player_a, sc_a=player_a_score,
-                                   pl_b=player_b, sc_b=player_b_score)
-        if result != 'result added':
-            print(f"Error: round: {rnd+1}, game: {game}, - {result}")
+for (idx, result) in results_data_df.iterrows():
+    if result.loc['played'] == 'y':
+        rnd = result.loc['round']
+        game = result.loc['game']
+        player_a = result.loc['player_a']
+        player_b = result.loc['player_b']
+        player_a_score = result.loc['player_a_sc']
+        player_b_score = result.loc['player_b_sc']
+        return_msg = league.add_result(rnd=rnd, game=game,
+                                       pl_a=player_a, sc_a=player_a_score,
+                                       pl_b=player_b, sc_b=player_b_score)
+        if return_msg != 'result added':
+            print(f"Error: round: {rnd+1}, game: {result}, - {result}")
 
-# export player data
-league.get_all_player_data().to_csv(filename+"_player_data.csv", index=False)
+# get dataframes from the league instance
+player_data_df = league.get_all_player_data()
+standings_df = league.get_standings()
+all_results_df = league.get_all_result_data()
+player_records_df = league.get_all_player_records()
 
-# export standings
-standings = league.get_standings()
-standings.to_csv(filename+"_standings.csv", index=False)
+# export csv files
+filename = league_name.replace(" ", "-")
+player_data_df.to_csv(f'output_data/{filename}_player_data.csv', index=False)
+standings_df.to_csv(f'output_data/{filename}_standings.csv', index=False)
+all_results_df.to_csv(f'output_data/{filename}_all_results.csv', index=False)
+player_records_df.to_csv(f'output_data/{filename}_player_records.csv', index=False)
 
-# export all results
-league.get_all_result_data().to_csv(filename+"_all_results.csv", index=False)
-
-# export player records
-league.get_all_player_records().to_csv(filename+"_records.csv", index=False)
-
-player_ls = standings['player'].to_list()
-points_ls = standings['league points'].to_list()
+# generate a readme file
+player_ls = standings_df['player'].to_list()
+points_ls = standings_df['league points'].to_list()
 
 for idx, player in enumerate(player_ls):
     print(f"{player} - {points_ls[idx]}")
